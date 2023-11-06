6b358f856fc15a8a5aef230fd762e2948cc76e31
samgcrypto <skyrealmoblivion@gmail.com>
2023-11-05 19:28:11 -0500

    Add project files.

---
+++ NFLTeams/Controllers/NameController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NFLTeams.Models;
+
+namespace NFLTeams.Controllers
+{
+    public class NameController : Controller
+    {
+        [HttpGet]
+        public ViewResult Index()
+        {
+            // get favorite teams and current conference and division
+            // from session, pass to view in view model
+            var session = new NFLSession(HttpContext.Session);
+            var model = new TeamsViewModel
+            {
+                Username = session.GetName(),
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv(),
+                Teams = session.GetMyTeams()
+            };
+
+            return View(model);
+        }
+        [HttpPost]
+        public RedirectToActionResult Change(TeamsViewModel model)
+        {
+            var session = new NFLSession(HttpContext.Session);
+            session.SetName(model.Username);
+
+            return RedirectToAction("Index", "Home",
+            new {
+                ActiveConf=session.GetActiveConf(),
+                ActiveDiv=session.GetActiveDiv(),
+            });
+        }
+        [HttpPost]
+        public RedirectToActionResult Delete(TeamsViewModel model)
+        {
+            var session = new NFLSession(HttpContext.Session);
+            session.RemoveMyTeams();
+
+            TempData["message"] = "Username is removed.";
+
+            return RedirectToAction("Index", "Home",
+            new
+            {
+                ActiveConf = session.GetActiveConf(),
+                ActiveDiv = session.GetActiveDiv(),
+            });
+        }
+
+
+    }
+}
